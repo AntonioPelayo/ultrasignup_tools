4054435d1bd721f7c60ba3da7a5ae3344e266d6e
Antonio Pelayo <aPelayo.py@gmail.com>
2024-01-31 16:28:59 -0800

    add web scraping functions, save_soup and tag_exists

--- ultrasignup_tools/web_scraping.py
+++ ultrasignup_tools/web_scraping.py
@@ -39,3 +39,31 @@ def get_webpage_soup(url):
         soup = BeautifulSoup(response.text, 'html.parser')
 
     return soup
+
+def save_soup(soup, path='soup.html'):
+    """
+    Save the soup object to a temporary HTML file.
+
+    Args:
+        soup (BeautifulSoup): The soup object.
+
+    Returns:
+        None
+    """
+    with open(path, 'w') as f:
+        f.write(str(soup))
+
+def tag_exists(soup, tag, attribute, identifier):
+    """
+    Check if the tag exists in the soup object.
+
+    Args:
+        soup (BeautifulSoup): The soup object.
+        tag (str): The tag type.
+        attribute (str): The tag attribute.
+        identifier (str): The tag identifier.
+
+    Returns:
+        bool: True if the tag exists, False otherwise.
+    """
+    return soup.find(tag, {attribute: identifier}) is not None
