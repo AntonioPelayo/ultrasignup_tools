cd1f6386b08287b1696e564e3a86e026b7ce6cea
Antonio Pelayo <aPelayo.py@gmail.com>
2024-10-19 23:44:56 -0700

    refactor: create EventEntrants class

--- ultrasignup_tools/event_entrants.py
+++ ultrasignup_tools/event_entrants.py
@@ -1,79 +1,90 @@
-HTML_TAGS = {
-    'entrants count': {
-        'tag': 'span',
-        'attribute': 'id',
-        'identifier': 'ContentPlaceHolder1_lblCount'
-    },
-    'entrants table': {
-        'tag': 'table',
-        'attribute': 'class',
-        'identifier': 'ultra_grid'
+class EventEntrants:
+    HTML_TAGS = {
+        'entrants count': {
+            'tag': 'span',
+            'attribute': 'id',
+            'identifier': 'ContentPlaceHolder1_lblCount'
+        },
+        'entrants table': {
+            'tag': 'table',
+            'attribute': 'class',
+            'identifier': 'ultra_grid'
+        }
     }
-}
 
-def get_entrants_count(soup):
-    """
-    Get the number of event entrants.
+    def __init__(self, soup):
+        self.entrants = self.get_entrants_table(soup)
+        self.entrants_count = self.get_entrants_count(soup)
 
-    Args:
-        soup (BeautifulSoup): The soup object.
+    def export_entrants_table(table, export_path):
+        """
+        Export the entrants table as a CSV file.
 
-    Returns:
-        int: The number of event entrants.
-    """
-    count = soup.find(
-        HTML_TAGS['entrants count']['tag'],
-        attrs={
-            HTML_TAGS['entrants count']['attribute'],
-            HTML_TAGS['entrants count']['identifier']
-        }
-    ).text
+        Args:
+            table (bs4.element.Tag): The entrants table.
+            export_path (str): The export path.
 
-    return [int(c) for c in count.split() if c.isdigit()][0]
+        Returns:
+            None
+        """
+        with open(export_path, 'w') as f:
+            f.write(','.join([
+                'Rank', 'Age Rank', 'Results', 'Target', 'Age', 'Has Trophy',
+                'First', 'Last', 'City', 'State', 'Empty', 'Bib', 'Finishes'
+            ]))
 
-def export_entrants_table(table, export_path):
-    """
-    Export the entrants table as a CSV file.
+            for row in table.find_all('tr'):
+                cells = row.find_all('td')
+                data = [
+                    cell.text.strip().replace(', ', ',')
+                    for cell in cells[:-1]
+                ]
+                f.write(','.join(data) + '\n')
 
-    Args:
-        table (bs4.element.Tag): The entrants table.
-        export_path (str): The export path.
+    @classmethod
+    def get_entrants_table(cls, soup, export=False, export_path='entrants.csv'):
+        """
+        Get the table of event entrants.
 
-    Returns:
-        None
-    """
-    with open(export_path, 'w') as f:
-        f.write(','.join(['Rank', 'Age Rank', 'Results', 'Target', 'Age', 'Has Trophy', 'First', 'Last', 'City', 'State', 'Empty', 'Bib', 'Finishes']))
-        for row in table.find_all('tr'):
-            cells = row.find_all('td')
-            data = [
-                cell.text.strip().replace(', ', ',')
-                for cell in cells[:-1]
-            ]
-            f.write(','.join(data) + '\n')
+        Args:
+            soup (BeautifulSoup): The soup object.
+            export (bool): Whether to export the table.
+            export_path (str): The export path.
 
-def get_entrants_table(soup, export=False, export_path='entrants.csv'):
-    """
-    Get the table of event entrants.
+        Returns:
+            bs4.element.Tag or None: The entrants table if not exporting.
+        """
+        table = soup.find(
+            cls.HTML_TAGS['entrants table']['tag'],
+            attrs={
+                cls.HTML_TAGS['entrants table']['attribute'],
+                cls.HTML_TAGS['entrants table']['identifier']
+            }
+        )
 
-    Args:
-        soup (BeautifulSoup): The soup object.
-        export (bool): Whether to export the table.
-        export_path (str): The export path.
+        if export:
+            cls.export_entrants_table(table, export_path)
+            return None
+        else:
+            return table
 
-    Returns:
-        bs4.element.Tag or None: The entrants table if not exporting.
-    """
-    table = soup.find(
-        HTML_TAGS['entrants table']['tag'],
-        attrs={
-            HTML_TAGS['entrants table']['attribute'],
-            HTML_TAGS['entrants table']['identifier']
-        }
-    )
+    @classmethod
+    def get_entrants_count(cls, soup):
+        """
+        Get the number of event entrants.
 
-    if export:
-        export_entrants_table(table, export_path)
-        return None
-    else:
-        return table
+        Args:
+            soup (BeautifulSoup): The soup object.
+
+        Returns:
+            int: The number of event entrants.
+        """
+        count = soup.find(
+            cls.HTML_TAGS['entrants count']['tag'],
+            attrs={
+                cls.HTML_TAGS['entrants count']['attribute'],
+                cls.HTML_TAGS['entrants count']['identifier']
+            }
+        ).text
+
+        return [int(c) for c in count.split() if c.isdigit()][0]
