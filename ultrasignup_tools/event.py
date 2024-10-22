d70d8153baf1c65158df471105f7bd9ea3212e55
Antonio Pelayo <aPelayo.py@gmail.com>
2024-10-21 21:57:08 -0700

    refactor: simplify UltraSignupEvent and improve attribute extraction logic

--- ultrasignup_tools/event.py
+++ ultrasignup_tools/event.py
@@ -1,79 +1,66 @@
+from .ultrasignup_endpoints import UltraSignupEndpoints
+from .web_scraping import find_item, get_webpage_soup
+
 class UltraSignupEvent:
     HTML_TAGS = {
-        'event title': {
+        'title': {
             'tag': 'h1',
             'attribute': 'class',
             'identifier': 'event-title'
         },
-        'event address': {
+        'address': {
             'tag': 'a',
             'attribute': 'class',
             'identifier': 'address_link'
         },
-        'event subtitle': {
+        'subtitle': {
             'tag': 'h2',
             'attribute': 'class',
             'identifier': 'subtitle'
         },
-        'event date': {
+        'date': {
             'tag': 'span',
             'attribute': 'id',
             'identifier': 'lblDate'
         },
-        'registration status': {
+        'registration_status': {
             'tag': 'span',
             'attribute': 'id',
             'identifier': 'ContentPlaceHolder1_EventInfoThin1_lblRegistrationStatus'
         },
     }
 
-    def __init__(self, id=None, soup=None):
-        self.id = id
-        self.soup = soup
-        self.info = self.get_event_info(soup)
+    def __init__(self, url):
+        self.url = url
+        self.id = UltraSignupEndpoints.event_id(url)
+        self.soup = get_webpage_soup(self.url)
+        self._set_attributes()
 
-    @classmethod
-    def get_event_info(cls, soup):
+    def _set_attributes(self):
         """
-        Get the event information from the soup object.
-
-        Args:
-            soup (BeautifulSoup): The soup object.
-
-        Returns:
-            dict: The event information.
+        Set the attributes for the event object.
         """
-        event_info = {}
-
-        for key, value in cls.HTML_TAGS.items():
-            try:
-                if key == 'event subtitle':
-                    city, events = cls.split_event_subtitle(
-                        soup.find(
-                            value['type'],
-                            attrs={
-                                value['attribute']:
-                                value['identifier']
-                            }
-                        ).text
+        for key, tag_info in self.HTML_TAGS.items():
+            if key == 'subtitle':
+                self.city, self.events = self._split_event_subtitle(find_item(
+                    self.soup,
+                    tag_info['tag'],
+                    tag_info['attribute'],
+                    tag_info['identifier']
+                ))
+            else:
+                setattr(
+                    self,
+                    key,
+                    find_item(
+                        self.soup,
+                        tag_info['tag'],
+                        tag_info['attribute'],
+                        tag_info['identifier']
                     )
-                    event_info['city'] = city
-                    event_info['events'] = [e.strip() for e in events.split(',')]
-                else:
-                    event_info[key] = soup.find(
-                        value['type'],
-                        attrs={
-                            value['attribute']:
-                            value['identifier']
-                        }
-                    ).text
-            except Exception as e:
-                print(e)
-                event_info[key] = None
+                )
 
-        return event_info
-
-    def _split_event_subtitle(subtitle):
+    def _split_event_subtitle(self, subtitle):
         """
         Split the event subtitle into its components.
 
@@ -86,3 +73,8 @@ class UltraSignupEvent:
         city, events = subtitle.split('•')
 
         return city.strip(), events.strip()
+
+    def __repr__(self):
+        attrs = {k: v for k, v in self.__dict__.items()}
+        attrs.pop('soup')
+        return f'{self.__class__.__name__}({attrs})'
